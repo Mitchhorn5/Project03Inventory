6ace1ce26fe9548bdadc703f55850bee17222b56
Mitchell <mrh190008@utdallas.edu>
2023-11-30 12:18:47 -0600

    Day 3 of Inventory Setup
    Refined inventory design. got inventory to show and hide. created item 1 and 2 sprite buttons

--- Assets/_Game/Scripts/ItemObject.cs
+++ Assets/_Game/Scripts/ItemObject.cs
@@ -3,44 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemObject : MonoBehaviour
-{/*
-    public InventoryItemData referenceItem;
-
-    public void OnHandlePickupItem()
-    {
-        InventorySystem.current.Add(referenceItem);
-        Destroy(gameObject);
-    }
-
-    void Update()
-    {
-        if(!useLookAt)
-        {
-            _targetPosition = _parent.position + _parent.forward * new Vector3(0f, 2f, 0f);
-        }
-
-        _lookAtTarget.transform.position = Vector3.Lerp(_lookAtTarget.transform.position, _targetPosition, Time.deltaTime * lookSpeed);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (_lookAtTarget != null && _lookAtTarget.TryGetComponent<ItemObject>(out ItemObject Item))
-            {
-                itemController.SetTargetPosition(Item.transform);
-                itemController.Pickup();
-            }
-        }
-    }
-
-    public void OnTriggerEnder(Collider collider)
-    {
-        if(collicer.transform.GetComponent<PointOfInterest>())
-        {
-            _targetPosition = collider.transform.position;
-            useLookAt = true;
-        }
-    }
-    public void OnTriggerExit(Collider collider)
-    {
-        useLookAt = false;
-    }*/
+{
+    //create an array with eath item i want to populate in my inventory
+    //give it visuals i can place in inventory canvas
+   //do in update
 }
